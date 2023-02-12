63f5088139f50007f305becaf19d007199b66642
sasa <sasa@mail.com>
2023-02-12 15:10:43 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,97 @@
+# ライブラリのインポート
+import preprocess
+import os
+import random
+import datetime 
+import numpy as np
+import pandas as pd
+from pandas_datareader import data
+import pandas_datareader.data as web
+# import matplotlib.pyplot as plt
+import yfinance as yf
+from sklearn.model_selection import train_test_split
+# 線形回帰モデルのLinearRegressionをインポート
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error as mse
+# 時系列分割のためTimeSeriesSplitのインポート
+from sklearn.model_selection import TimeSeriesSplit
+yf.pdr_override()
+
+# 入力された値を取得
+stock_code = "4923"
+stock_code = str(stock_code)+".T"
+
+# 本日の日時を取得
+dt_now = datetime.datetime.now()
+
+# 情報の取得開始日を指定する
+start = '2019-02-01'
+
+# 情報の取得終了日を指定する
+end = datetime.date.today()
+if dt_now.weekday() == 0:
+    end = end + datetime.timedelta(days=-3)
+elif dt_now.weekday() == 6:
+    end = end + datetime.timedelta(days=-2)
+elif dt_now.weekday() == 5:
+    end = end + datetime.timedelta(days=-1)   
+elif dt_now.hour<20 and 0<dt_now.weekday()<5:
+    end = end + datetime.timedelta(days=-1)
+
+# 日経平均などの説明変数準備
+files = os.listdir("./add_data/")
+if len(files)>1:
+    for file in files:
+        os.remove("./add_data/{}".format(file))
+is_file = os.path.isfile("./add_data/{}_add_data.pickle".format(end))
+if not is_file:
+    preprocess.mk_adddata(start,end)
+add_data = pd.read_pickle("./add_data/{}_add_data.pickle".format(end)) 
+print(add_data)
+
+# 株の取得
+data_master = yf.download(tickers=stock_code, start=start,interval = "1d")
+
+# 株データの加工
+data_technical = preprocess.stock_preprocess(data_master)
+
+# 株データに説明変数の追加
+data_technical = pd.merge(data_technical, add_data, on="Date", how="inner")
+data_technical.index = pd.to_datetime(data_technical.index)
+
+# 2018年〜2020年を学習用データとする
+train = data_technical[start : '2022-05-31']
+# 2021年をテストデータとする
+test = data_technical['2022-06-01' :]
+
+# 学習用データとテストデータそれぞれを説明変数と目的変数に分離する
+X_train = train.drop(columns=['y']) #学習用データ説明変数
+y_train = train['y'] #学習用データ目的変数
+
+X_test = test.drop(columns=['y']) #テストデータ説明変数
+y_test = test['y'] #テストデータ目的変数
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+# 実際の予測結果確認
+df_result = test
+df_result['Close_pred'] = y_pred
+
+df_result['Pred_diff'] = df_result['Close_pred'].diff(1)
+print(df_result[['Pred_diff','Close_pred']].tail(10))
+print(df_result['Pred_diff'].tail(1))
+if df_result['Pred_diff'].tolist().pop()>0 and df_result['Close_pred'].tolist().pop()>=0.25:
+    message = "期待大（参考値　pred:{}）".format(df_result['Close_pred'].tolist().pop())
+elif df_result['Pred_diff'].tolist().pop()>0 and 0<df_result['Close_pred'].tolist().pop()<0.25:
+    message = "期待中（参考値　pred:{}）".format(df_result['Close_pred'].tolist().pop())
+elif df_result['Pred_diff'].tolist().pop()<=0 and 0<df_result['Close_pred'].tolist().pop()<0.25:
+    message = "期待小（参考値　pred:{}）".format(df_result['Close_pred'].tolist().pop())
+else:
+    message = "期待なし（参考値　pred:{}）".format(df_result['Close_pred'].tolist().pop())
+
+print(message)
+
+
+
---
+++ preprocess.py
@@ -0,0 +1,69 @@
+
+# ライブラリのインポート
+import datetime
+import numpy as np
+import pandas as pd
+from pandas_datareader import data
+# import matplotlib.pyplot as plt
+import yfinance as yf
+
+import pandas_datareader.data as web
+
+yf.pdr_override()
+
+# add_column = ["USDJPY=X","^N225","^DJI","^GSPC","998405.T"]
+add_column = ["USDJPY=X","^N225","998405.T"]
+
+# 移動平均を追加
+SMA1 = 3   #短期3日
+SMA2 = 5  #中期5日
+SMA3 = 10  #長期10日
+
+def mk_adddata(start,end):
+    i=0
+    for val in add_column:
+        temp = web.get_data_yahoo(val, start)
+        temp.drop('Volume', axis=1,inplace=True)
+        temp = temp.add_prefix(val+"_")
+        temp.dropna(inplace=True)
+        temp.rename(index = lambda x:x.date(),inplace=True)
+        if i == 0:
+            add_data = temp
+        else:
+            if temp.tail(1).index == end:
+                add_data = pd.merge(add_data, temp, on="Date", how="inner")
+            else:
+                print("b")
+        i += 1
+    add_data.to_pickle("./add_data/{}_add_data.pickle".format(end))
+
+def stock_preprocess(df):
+
+    df['weekday'] = df.index.weekday
+    df.rename(index = lambda x:x.date(),inplace=True)
+
+    df = df.dropna(how='any')
+
+    df['SMA1'] = df['Close'].rolling(SMA1).mean() #短期移動平均の算出
+    df['SMA2'] = df['Close'].rolling(SMA2).mean() #中期移動平均の算出
+    df['SMA3'] = df['Close'].rolling(SMA3).mean() #長期移動平均の算出
+
+    # OpenとCloseの差分を実体Bodyとして計算
+    df['Body'] = df['Open'] - df['Close']
+    # 前日終値との差分Close_diffを計算
+    df['Close_diff'] = df['Close'].diff(1)
+    # 目的変数となる翌日の終値Close_nextの追加
+    df['Close_next'] = df['Close'].shift(-1)
+    df['y_'] = 100*(df['Close_next']-df['Close'])/df['Close']
+    df['y'] = df['y_'].apply(lambda x:1 if x > 2 else 0)
+
+    # 不要な目的変数削除
+    df.drop(columns=['Close_next','y_'],inplace=True)
+
+    # 欠損値がある行を削除
+    # df = df.dropna(how='any')
+    df = df.fillna(method='ffill')
+    df = df.fillna(method='bfill')
+    return df
+
+
