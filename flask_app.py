0dc8ccf9847ff7b903bc4755dedf1fc7dc8135d2
sasa <sasa@mail.com>
2023-02-28 08:48:31 +0900

    line機能修正

--- flask_app.py
+++ flask_app.py
@@ -36,6 +36,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    rank,threshold = pred.predict(event.message.text)
+    message = pred.classify(rank,threshold)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=pred.predict(event.message.text))
